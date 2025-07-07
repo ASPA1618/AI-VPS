9c065d2612f678a19f1d475847818c740422027f
ASPA1618 <213491853+ASPA1618@users.noreply.github.com>
2025-07-07 17:32:43 +0300

    Create config_utils.py

---
+++ config_utils.py
@@ -0,0 +1,11 @@
+import json
+
+CONFIG_PATH = "config.json"
+
+def load_config():
+    with open(CONFIG_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_config(config):
+    with open(CONFIG_PATH, "w", encoding="utf-8") as f:
+        json.dump(config, f, ensure_ascii=False, indent=4)
