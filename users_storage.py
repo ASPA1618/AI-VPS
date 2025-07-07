158cbeac90a0e8b9876ac02f179b615e1d25aca1
ASPA1618 <213491853+ASPA1618@users.noreply.github.com>
2025-07-07 23:43:22 +0300

    Create users_storage.py

---
+++ users_storage.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+USERS_PATH = "users.json"
+
+def load_users():
+    if not os.path.exists(USERS_PATH):
+        return {}
+    with open(USERS_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_users(users):
+    with open(USERS_PATH, "w", encoding="utf-8") as f:
+        json.dump(users, f, ensure_ascii=False, indent=2)
